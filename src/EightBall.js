e1dee35a98c3e001760aa78157285581e1f46317
Saif <saifisami2005@gmail.com>
2024-12-05 19:32:52 -0500

    Finished eightball implementation.

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,17 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const EightBall = ({answers}) => {
-    const [state, setState] = useState({msg: "Think of a Question", color: "black"});
-    const ballStyle = {backgroundColor: state.color};
+const EightBall = ({ answers }) => {
+    const [state, setState] = useState({ msg: "Think of a Question", color: "black" });
+    const ballStyle = {
+        backgroundColor: state.color,
+        color: "white",
+        width: "200px",
+        height: "200px",
+        borderRadius: "50%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        cursor: "pointer",
+        margin: "0 auto",
+        fontSize: "1.5rem",
+    };
+
     const handleClick = () => {
         let ansIdx = Math.floor(Math.random() * answers.length);
         let ans = answers[ansIdx]
-        setState({msg: ans.msg, color: ans.color})
+        setState({ msg: ans.msg, color: ans.color })
     }
     return (
-        <div className={ballStyle}
-             onClick={handleClick}>
-                {state.msg};
+        <div className='EightBall'
+            style={ballStyle}
+            onClick={handleClick}>
+            {state.msg}
         </div>
     )
 }
